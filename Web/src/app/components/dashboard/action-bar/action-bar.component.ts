753426bf73e825ce9ef4821e8929b0700deaf697
Ariny Guedes <arinyguedes@gmail.com>
2018-01-24 22:15:39 -0200

    Control logged user data

--- Web/src/app/components/dashboard/action-bar/action-bar.component.ts
+++ Web/src/app/components/dashboard/action-bar/action-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Projections } from "../../../model/portfolio/projections";
 import { PortfolioDistribution } from "../../../model/portfolio/portfolioDistribution";
 import { PortfolioHistory } from "../../../model/portfolio/portfolioHistory";
+import { LoginService } from '../../../services/login.service';
 
 @Component({
   selector: 'action-bar',
@@ -12,10 +13,15 @@ export class ActionBarComponent implements OnInit {
   @Input() projectionsModel: Projections;
   @Input() portfolioDistributionModel: PortfolioDistribution;
   @Input() portfolioHistoryModel: PortfolioHistory;
+  @Input() user: string = null;
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit() {
+    this.user = this.loginService.getUser();
   }
 
+  logout(): void {
+    this.loginService.logout();
+  }
 }
