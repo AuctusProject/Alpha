8a1835039f0c19810489a407fc6128559f84c539
Guilherme Gusman <guilherme.gusman@lg7.com.br>
2018-03-21 20:20:56 -0300

    making eth balance method open

--- Node/app/helpers/authenticationHelper.js
+++ Node/app/helpers/authenticationHelper.js
@@ -6,10 +6,15 @@ module.exports = function (app) {
   'use strict';
 
   var authentication = function (req, res, next) {
-    if (!req.headers["x-api-key"] || req.headers["x-api-key"] != config.get('API_KEY')) {
+    if (req.url.indexOf("api/v1/address") >= 0){
+      next();
+    }
+    else if (!req.headers["x-api-key"] || req.headers["x-api-key"] != config.get('API_KEY')) {
       throw new Error(401, 'request unauthorized');
     }
-    next(); 
+    else {
+      next();
+    } 
   }
 
   app.use(authentication);
